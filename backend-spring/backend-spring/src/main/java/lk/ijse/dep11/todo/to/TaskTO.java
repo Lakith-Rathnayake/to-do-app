165014bfb8c2df040cc16300303e399eac51c90f
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-12-26 02:58:26 +0530

    Complete request handler methods with business logics

--- backend-spring/backend-spring/src/main/java/lk/ijse/dep11/todo/to/TaskTO.java
+++ backend-spring/backend-spring/src/main/java/lk/ijse/dep11/todo/to/TaskTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.*;
+import javax.validation.groups.Default;
 import java.io.Serializable;
 
 @Data
@@ -15,9 +16,14 @@ public class TaskTO implements Serializable {
     private Integer id;
     @NotBlank(message = "Description should not be empty")
     private String description;
-    @NotNull(message = "Status should not be empty")
+    @NotNull(message = "Status should not be empty", groups = Update.class)
+    @Null(message = "Status should be empty", groups = Create.class)
     private Boolean status;
     @NotEmpty(message = "Email can't be empty")
     @Email
     private String email;
+
+    public interface Update extends Default{}
+
+    public interface Create extends Default{}
 }

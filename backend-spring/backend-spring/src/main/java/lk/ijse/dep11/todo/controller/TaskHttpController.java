5835f28c9a960aadff80cfbc3ade2a0bacf02d8f
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-12-25 23:41:57 +0530

    Add requst handler methods to TaskHttpController class

--- backend-spring/backend-spring/src/main/java/lk/ijse/dep11/todo/controller/TaskHttpController.java
+++ backend-spring/backend-spring/src/main/java/lk/ijse/dep11/todo/controller/TaskHttpController.java
@@ -1,11 +1,33 @@
 package lk.ijse.dep11.todo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/tasks")
 @CrossOrigin
 public class TaskHttpController {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void createTask() {
+        System.out.println("createTask()");
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping
+    public void updateTask() {
+        System.out.println("updateTask");
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping
+    public void deleteTask() {
+        System.out.println("deleteTask");
+    }
+
+    @GetMapping
+    public void getAllTasks() {
+        System.out.println("getAllTasks");
+    }
 }

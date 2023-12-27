67b58256fa01c8ac63fdebc39a09b092c417ad19
Lakith Rathnayake <lakithpathmila@gmail.com>
2023-12-28 03:51:24 +0530

    Implement saveTask()

--- frontend-react/src/service/task-service.ts
+++ frontend-react/src/service/task-service.ts
@@ -1,5 +1,17 @@
+import {TaskDTO} from "../dto/TaskDTO.ts";
+
 const API_BASE_URL = "http://localhost:8080/api/v1/tasks"
 
 export async function getAllTasks(email: string) {
     return await (await fetch(`${API_BASE_URL}?email=${email}`)).json();
+}
+
+export async function saveTask(task: TaskDTO) {
+    return await (await fetch(`${API_BASE_URL}`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(task)
+    })).json() as TaskDTO;
 }
